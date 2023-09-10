cf09510646f8637da199c2eaf303e389a5bb49d7
Michael Enion <caewok_2000@yahoo.com>
2023-09-10 15:54:27 -0700

    Add listeners for button presses.

--- scripts/TerrainSettingsMenu.js
+++ scripts/TerrainSettingsMenu.js
@@ -7,7 +7,7 @@ game
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 "use strict";
 
-import { MODULE_ID, LABELS  } from "./const.js";
+import { MODULE_ID, LABELS } from "./const.js";
 
 /**
  * Settings submenu for defining terrains.
@@ -29,10 +29,8 @@ export class TerrainSettingsMenu extends FormApplication {
   getData(options={}) {
     const data = super.getData(options);
     return foundry.utils.mergeObject(data, {
-      displayOptions: LABELS.DISPLAY_OPTIONS,
       rangeOptions: LABELS.RANGE_OPTIONS,
       visibilityOptions: LABELS.VISIBILITY_OPTIONS
-
     });
   }
 
@@ -40,4 +38,45 @@ export class TerrainSettingsMenu extends FormApplication {
     const expandedFormData = expandObject(formData);
   }
 
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    html.find("button.tm-toggle-visibility").click(this._onToggleVisibility.bind(this));
+    html.find("button.tm-edit-ae").click(this._onEditActiveEffect.bind(this));
+    html.find("button.tm-remove-terrain").click(this._onRemoveTerrain.bind(this));
+
+    html.find("button.tm-add-terrain").click(this._onAddTerrain.bind(this));
+    html.find("button.import").click(this._onImport.bind(this));
+    html.find("button.export").click(this._onExport.bind(this));
+  }
+
+  _onAddTerrain(event) {
+    event.preventDefault();
+    console.debug("addTerrain clicked!");
+  }
+
+  _onRemoveTerrain(event) {
+    event.preventDefault();
+    console.debug("removeTerrain clicked!");
+  }
+
+  _onEditActiveEffect(event) {
+    event.preventDefault();
+    console.debug("edit active effect clicked!");
+  }
+
+  _onToggleVisibility(event) {
+    event.preventDefault();
+    console.debug("visibility toggle clicked!");
+  }
+
+  _onImport(event) {
+    event.preventDefault();
+    console.debug("import clicked!");
+  }
+
+  _onExport(event) {
+    event.preventDefault();
+    console.debug("export clicked!");
+  }
 }
