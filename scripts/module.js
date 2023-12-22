3ce68d15cc60c404878f4701ef65ef8f0450476c
Michael Enion <caewok_2000@yahoo.com>
2023-12-22 10:22:19 -0800

    Add elevation to tile config

--- scripts/module.js
+++ scripts/module.js
@@ -14,6 +14,7 @@ import { TerrainMap } from "./TerrainMap.js";
 import { EffectHelper } from "./EffectHelper.js";
 import { PATCHER, initializePatching } from "./patching.js";
 import { registerGeometry } from "./geometry/registration.js";
+import { registerElevationConfig } from "./geometry/elevation_configs.js";
 import { terrainEncounteredDialog, updateTokenDocument } from "./Token.js";
 
 import { TerrainLayerShader } from "./glsl/TerrainLayerShader.js";
@@ -46,6 +47,7 @@ Hooks.once("init", function() {
  */
 Hooks.once("setup", function() {
   Settings.registerAll();
+  registerElevationConfig("TileConfig", "Terrain Mapper");
 });
 
 /**
