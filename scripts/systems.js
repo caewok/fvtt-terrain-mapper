d6c0f6d5bd5b70264b22ebec3f18be6b235c7f36
Jonas Karlsson <xdy@users.noreply.github.com>
2024-05-18 10:26:29 +0200

    Add support for Dragonbane system (https://github.com/pafvel/dragonbane/)

--- scripts/systems.js
+++ scripts/systems.js
@@ -24,6 +24,7 @@ export function getDefaultSpeedAttribute() {
     case "splittermond": return "actor.derivedValues.speed.value";
     case "wfrp4e": return "actor.system.details.move.walk";
     case "crucible": return "actor.system.movement.stride";
+    case "dragonbane":    return "actor.system.movement";
   }
   return "";
 }
