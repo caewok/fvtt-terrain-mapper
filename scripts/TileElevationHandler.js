97a151dfa7849f89e9538fd65b9da5cdfdc93bd7
Michael Enion <caewok_2000@yahoo.com>
2024-07-28 09:36:02 -0700

    💡 refactor|ElevatedTile|Separate method that can be used by worker

--- scripts/TileElevationHandler.js
+++ scripts/TileElevationHandler.js
@@ -334,18 +334,38 @@ export class TileElevationHandler {
     // - Take lowest, ignoring pixels outside border. Add 1 and set pixel to this sum.
     // - If changed, flag because the 8 neighbors must be retested after iteration is finished.
     // Until no more changes: update the 8 neighbors of each changed pixel.
-    const MAX_VALUE = 65535; // Uint16Array maximum.
     const { alphaThreshold, tile } = this;
     const tileCache = tile.evPixelCache;
     const holeCache = tileCache.constructor.fromOverheadTileAlpha(tile);
-    const nPixels = tileCache.pixels.length;
-    holeCache.pixels = new Uint16Array(nPixels);
 
     // Set each alpha pixel to the max integer value to start, 0 otherwise.
     console.group(`${MODULE_ID}|constructHoleCache ${this.tile.id}`);
-    const threshold = tileCache.maximumPixelValue * alphaThreshold;
+    const alphaPixelThreshold = tileCache.maximumPixelValue * alphaThreshold;
+    holeCache.pixels = this.#calculateHoleCachePixels(tileCache.pixels, tileCache.localFrame.width, alphaPixelThreshold);
+    console.groupEnd(`${MODULE_ID}|constructHoleCache ${this.tile.id}`);
+    return holeCache;
+    // avgPixels(holeCache.pixels); // 1.33
+    // drawPixels(holeCache)
+    // drawHoles(holeCache)
+    // pixelCounts(holeCache, max = 10)
+  }
+
+  /**
+   * Placeholder for eventual worker method that takes the tile array and constructs a hole array
+   * @param {Uint8Array} tileCachePixels      The pixel array for a tile
+   * @param {number} width                    The local width of the tile cache
+   * @param {number} alphaPixelThreshold      Value between 0 and 255; above this is non-transparent.
+   *                                          Default is alphaThreshold = 75%
+   * @returns {Uint16Array} The hole cache array
+   */
+  #calculateHoleCachePixels(tileCachePixels, width, alphaPixelThreshold = 191.25) {
+    const MAX_VALUE = 65535; // Uint16Array maximum.
+    const nPixels = tileCachePixels.length;
+    const holeCachePixels = new Uint16Array(nPixels);
+
+    // Set each alpha pixel to the max integer value to start, 0 otherwise.
     console.time(`${MODULE_ID}|Mark each alpha pixel`);
-    for ( let i = 0; i < nPixels; i += 1 ) holeCache.pixels[i] = tileCache.pixels[i] > threshold ? 0 : MAX_VALUE;
+    for ( let i = 0; i < nPixels; i += 1 ) holeCachePixels[i] = tileCachePixels[i] > alphaPixelThreshold ? 0 : MAX_VALUE;
     console.timeEnd(`${MODULE_ID}|Mark each alpha pixel`); // 6.5 ms.
     // avgPixels(holeCache.pixels); // 0.66
     // drawPixels(holeCache)
@@ -353,20 +373,19 @@ export class TileElevationHandler {
     // pixelCounts(holeCache, max = 1) // {0: 616231, 1: 0, > 1: 1408769, numPixels: 2025000}
 
     const changedIndices = new Set();
-    const updatePixel = (cache, idx) => {
-      const value = cache.pixels[idx];
+    const updatePixel = idx => {
+      const value = holeCachePixels[idx];
       if ( !value ) return;
-      const newValue = Math.min(MAX_VALUE, Math.min(...cache.localNeighbors(idx)) + 1);
+      const newValue = Math.min(MAX_VALUE, Math.min(...localNeighbors(holeCachePixels, idx, width)) + 1);
       if ( value === newValue ) return;
-      cache.pixels[idx] = newValue;
+      holeCachePixels[idx] = newValue;
       changedIndices.add(idx);
     }
 
     // For each pixel that is greater than 0, its value is 1 + min of 8 neighbors.
     // Record changed indices so we can re-process those neighbors.
-
     console.time(`${MODULE_ID}|Iterate over every pixel`);
-    for ( let i = 0; i < nPixels; i += 1 ) updatePixel(holeCache, i);
+    for ( let i = 0; i < nPixels; i += 1 ) updatePixel(i);
     console.timeEnd(`${MODULE_ID}|Iterate over every pixel`); // 100 ms
     // avgPixels(holeCache.pixels); // 1.33
     // drawPixels(holeCache)
@@ -381,22 +400,14 @@ export class TileElevationHandler {
       const indices = [...changedIndices.values()];
       changedIndices.clear();
       for ( const idx of indices ) {
-        const neighborIndices = holeCache.localNeighborIndices(idx);
-        for ( const neighborIdx of neighborIndices ) updatePixel(holeCache, neighborIdx);
+        const neighborIndices = localNeighborIndices(holeCachePixels, idx, width);
+        for ( const neighborIdx of neighborIndices ) updatePixel(neighborIdx);
       }
     }
     console.timeEnd(`${MODULE_ID}|Update pixels`); // 28801.6630859375 ms // 11687.419189453125 ms using pixelStep instead of x,y.
     console.log(`${MODULE_ID}|${iter} iterations.`);
-    console.groupEnd(`${MODULE_ID}|constructHoleCache ${this.tile.id}`);
-    return holeCache;
-    // avgPixels(holeCache.pixels); // 1.33
-    // drawPixels(holeCache)
-    // drawHoles(holeCache)
-    // pixelCounts(holeCache, max = 10)
+    return holeCachePixels;
   }
-
-
-
 
   // ----- NOTE: Debugging ----- //
 
@@ -467,3 +478,46 @@ TileElevationHandler.js:388 terrainmapper|Update pixels: 1013.5439453125 ms
 TileElevationHandler.js:389 terrainmapper|280 iterations.
 
 */
+
+/**
+ * For this rectangular frame of local pixels, step backward or forward in the x and y directions
+ * from a current index. Presumes index is row-based, such that:
+ * 0 1 2 3
+ * 4 5 6 7...
+ * @param {number} currIdx
+ * @param {number} [xStep = 0]
+ * @param {number} [yStep = 0]
+ * @returns {number} The new index position
+ */
+function localPixelStep(currIdx, localWidth, xStep = 0, yStep = 0) {
+  return currIdx + (yStep * localWidth) + xStep;
+}
+
+/**
+ * Indices of the 8 neighbors to this local pixel index. Does not
+ * @param {number} currIdx
+ * @returns {number[]}
+ */
+function localNeighborIndices(pixels, currIdx, localWidth, trimBorder = true) {
+  const arr = [];
+  const maxIdx = pixels.length - 1;
+  for ( let xi = -1; xi < 2; xi += 1 ) {
+    for ( let yi = -1; yi < 2; yi += 1 ) {
+      if ( !(xi || yi) ) continue;
+      const neighborIdx = localPixelStep(currIdx, localWidth, xi, yi);
+      if ( trimBorder && !neighborIdx.between(0, maxIdx) ) continue;
+      arr.push(neighborIdx);
+    }
+  }
+  return arr;
+}
+
+/**
+ * Retrieve the 8 neighbors to a given index on the local cache.
+ * @param {number} currIdx
+ * @param {boolean} [trimBorder=true]    If true, exclude the border values
+ * @returns {number[]} The values, in column order, skipping the middle value.
+ */
+function localNeighbors(pixels, currIdx, localWidth, trimBorder = true) {
+  return localNeighborIndices(pixels, currIdx, localWidth, trimBorder).map(idx => pixels[idx]);
+}
