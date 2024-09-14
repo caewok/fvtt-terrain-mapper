b794332abd15a6524cb2df3395aab036283efd0d
Michael Enion <caewok_2000@yahoo.com>
2024-09-14 15:18:55 -0400

    WIP|SceneGraph|WIP to add SceneGraph to libGeometry
    Doesn't really work b/c cycle polygons don't work if you are excluding some of the graph (i.e. token-only edges)

--- scripts/WallTracer.js
+++ scripts/WallTracer.js
@@ -22,6 +22,56 @@ import { Settings } from "./settings.js";
 import { doSegmentsOverlap, IX_TYPES, segmentCollision } from "./geometry/util.js";
 import { MODULE_ID } from "./const.js";
 
+export class ERSceneGraph extends SceneGraph {
+
+  /**
+   * Could edges of this token block the moving token?
+   * @param {Token} token             Token whose edges will be tested
+   * @param {Token} moveToken         Token doing the move
+   * @param {number} [elevation=0]  Elevation of the point or origin to test.
+   * @param {string} tokenBlockType   What test to use for comparing token dispositions for blocking
+   * @returns {boolean}
+   */
+  static tokenEdgeBlocks(token, moveToken, elevation = 0, tokenBlockType) {
+    if ( !SceneGraph.tokenEdgeBlocks(token, moveToken, elevation) ) return false;
+
+    // Don't block dead tokens (HP <= 0).
+    const { tokenHPAttribute, pathfindingIgnoreStatuses } = CONFIG[MODULE_ID];
+    const tokenHP = Number(foundry.utils.getProperty(token, tokenHPAttribute));
+    if ( Number.isFinite(tokenHP) && tokenHP <= 0 ) return false;
+
+    // Don't block tokens with certain status.
+    if ( token.actor?.statuses && token.actor.statuses.intersects(pathfindingIgnoreStatuses) ) return false;
+
+    // Don't block tokens that share specific disposition with the moving token.
+    tokenBlockType ??= Settings._tokenBlockType();
+    const D = CONST.TOKEN_DISPOSITIONS;
+    const moveTokenD = moveToken.document.disposition;
+    const edgeTokenD = token.document.disposition;
+    switch ( tokenBlockType ) {
+      case D.NEUTRAL: return false;
+      case D.SECRET: return true;
+
+      // Hostile: Block if dispositions are different
+      case D.HOSTILE: return ( edgeTokenD === D.SECRET
+        || moveTokenD === D.SECRET
+        || edgeTokenD !== moveTokenD );
+
+      // Friendly: Block if dispositions are the same
+      case D.FRIENDLY: return ( edgeTokenD === D.SECRET
+        || moveTokenD === D.SECRET
+        || edgeTokenD === moveTokenD );
+
+      default: return true;
+    }
+  }
+
+}
+
+
+
+
+
 /* WallTracerVertex
 
 Represents the endpoint of a WallTracerEdge.
@@ -996,6 +1046,24 @@ export class WallTracer extends Graph {
 
 // Must declare this variable after defining WallTracer.
 export const SCENE_GRAPH = new WallTracer();
+
+
+/**
+ * Class that can be extended to handle special edge tests while still using the underlying scene graph.
+ */
+export SceneGraph extends WallTracer {
+  /** @type {WallTracer} */
+  SCENE_GRAPH = SCENE_GRAPH;
+
+  constructor() {
+
+  }
+
+
+
+}
+
+
 
 /* Debugging
 api = game.modules.get("elevationruler").api
